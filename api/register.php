11e27dcf218cbcb92eb0a6234e5d9c9134ae626e
nutpanda18 <nutpanda18@gmail.com>
2026-05-24 13:53:15 +0700

    editing

--- api/register.php
+++ api/register.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Daftar Akun Baru - BukuKasPro</title>
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body class="landing-body">
 
